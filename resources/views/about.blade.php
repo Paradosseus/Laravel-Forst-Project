12b6d8391443e3e7e3867fa27536b4c7d6f4a3a9
Paradosseus <colladofrancis524@gmail.com>
2023-01-17 02:10:28 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsam sed error vero. Aliquid quibusdam animi voluptatem, modi harum molestias, quaerat voluptates ipsa autem quo facere distinctio optio assumenda nulla.</p>
+</body>
+</html>
---
+++ resources/views/contact_us.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Contact us</title>
+</head>
+<body>
+    <h1>Contact Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum non, aspernatur dolor esse voluptates quae eaque dignissimos, asperiores corrupti, odit maxime magnam quod iure iste aliquid voluptatibus blanditiis architecto? Voluptatum.</p>
+</body>
+</html>
